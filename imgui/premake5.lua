b1300f164f3c36f381d210a6adb499505bcbfe51
GilbertNordhammar <gilbertnordhammar@gmail.com>
2021-01-02 15:10:17 +0100

    Syncing/im gui docking branch (#5)
    * updating imgui to beta branch 'docking'
    
    * updated premake config

--- imgui/premake5.lua
+++ imgui/premake5.lua
@@ -20,16 +20,17 @@ project "ImGui"
         "imgui_demo.cpp",
         "imgui_draw.cpp",
         "imgui_internal.h",
+        "imgui_tables.cpp",
         "imgui_widgets.cpp",
         "imgui.cpp",
         "imgui.h",
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-        "examples/imgui_impl_opengl3.cpp",
-        "examples/imgui_impl_opengl3.h",
-        "examples/imgui_impl_glfw.cpp",
-        "examples/imgui_impl_glfw.h"
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_glfw.h"
     }
     
 	filter "system:windows"
